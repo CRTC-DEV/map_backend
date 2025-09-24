9e1a075358f7e2ac5c2ffbde8a530bb1d3672da7
locphamquang <loc.pq@camranh.aero>
2025-09-24 10:38:25 +0700

    create Get Map Item full

--- app/Http/Controllers/API/MapItemAPI.php
+++ app/Http/Controllers/API/MapItemAPI.php
@@ -15,39 +15,20 @@ use App\Models\Map\MapItem;
 class MapItemAPI extends Component
 {
     
-
-    /**
-     * @OA\Get(
-     *     path="/api/mapitem/{floor}",
-     *     tags={"Map Items"},
-     *     summary="Get map items by floor number",
-     *     @OA\Parameter(
-     *         name="floor",
-     *         in="path",
-     *         required=true,
-     *         description="Floor number",
-     *         @OA\Schema(type="integer")
-     *     ),
-     *     @OA\Response(
-     *         response=200,
-     *         description="Successful operation",
-     *         @OA\JsonContent(
-     *             type="object",
-     *             @OA\Property(property="status", type="string", example="success"),
-     *             @OA\Property(
-     *                 property="data",
-     *                 type="array",
-     *                 @OA\Items(ref="#/components/schemas/MapItem")
-     *             )
-     *         )
-     *     )
-     * )
-     */
-
     public function getMapItem($floor)
     {
         $obj_items = new MapItem();
-        //$items = $obj_items->getItemDescriptionAPI($catId, $languageid,$floor);
+        $items = $obj_items->getAllMapItemsAPI($floor);
+        return response()->json([
+            'status' => 'success',
+            'data' => $items
+        ]);
+    }
+    public function postMapItem(Request $request)
+    {
+        $floor = $request->input('floor');
+        $apiKey = $request->header('X-API-Key');
+        $obj_items = new MapItem();
         $items = $obj_items->getAllMapItemsAPI($floor);
         //dd($items);
         return response()->json([
@@ -56,53 +37,11 @@ class MapItemAPI extends Component
         ]);
     }
 
-    /**
-     * @OA\Post(
-     *     path="/api/mapitem",
-     *     tags={"Map Items"},
-     *     summary="Get map items ",
-     *     @OA\RequestBody(
-     *         required=true,
-     *         @OA\JsonContent(
-     *             required={"floor"},
-     *             @OA\Property(property="floor", type="integer", example=1)
-     *         )
-     *     ),
-     *     @OA\Response(
-     *         response=200,
-     *         description="Successful operation",
-     *         @OA\JsonContent(
-     *             type="object",
-     *             @OA\Property(property="status", type="string", example="success"),
-     *             @OA\Property(
-     *                 property="data",
-     *                 type="array",
-     *                 @OA\Items(ref="#/components/schemas/MapItem")
-     *             )
-     *         )
-     *     ),
-     *     @OA\Response(
-     *         response=401,
-     *         description="Unauthorized"
-     *     )
-     * )
-     */
-    public function postMapItem(Request $request)
+    // get full map item without floor
+    public function getMapItemFull()
     {
-        // get data from body
-        $floor = $request->input('floor');
-        // get data from header
-        $apiKey = $request->header('X-API-Key');
-        // check api key
-        // if (!$apiKey) {
-        //     return response()->json([
-        //         'status' => 'error',
-        //         'message' => 'API key is required'
-        //     ], 401);
         $obj_items = new MapItem();
-        //$items = $obj_items->getItemDescriptionAPI($catId, $languageid,$floor);
-        $items = $obj_items->getAllMapItemsAPI($floor);
-        //dd($items);
+        $items = $obj_items->getAllMapItemsFull();
         return response()->json([
             'status' => 'success',
             'data' => $items
